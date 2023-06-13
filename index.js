c78e88d8db9b0db09c0fbb8da78692b7289cdf5f
Abogeerick <abogeerick@gmail.com>
2023-06-13 05:48:07 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let message;
+  
+  if (distance <= 400) {
+    message = "This one is on me!";
+  } else if (distance > 2000 && distance <= 2500) {
+    message = "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    message = "No can do.";
+  }
+  
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message;
+  if (city === "NYC"){
+    message ="Ok, sounds good."
+  } else if(city === "Pittsburgh"){
+    message = "No go."
+  }
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
